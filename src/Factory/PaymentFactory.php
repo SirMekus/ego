457e83828a7900f9875f597b18c5073aa35eaf07
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-11-16 05:11:52 +0100

    fix: corrected misspelled filename and class.

--- src/Factory/PaymentFactory.php
+++ src/Factory/PaymentFactory.php
@@ -4,32 +4,30 @@ namespace Emmy\Ego\Factory;
 
 use Illuminate\Http\Request;
 use Emmy\Ego\Interface\PaymentGatewayInterface;
-use Emmy\Ego\Exception\UnspportedGatewayException;
+use Emmy\Ego\Exception\UnsupportedGatewayException;
 
 class PaymentFactory implements PaymentGatewayInterface
 {
     protected $paymentGateway;
 
     public function __construct(
-        string|PaymentGatewayInterface $gateway="",
-        string|array $key = "")
-	{
-        if($gateway instanceof PaymentGatewayInterface){
+        string|PaymentGatewayInterface $gateway = "",
+        string|array $key = ""
+    ) {
+        if ($gateway instanceof PaymentGatewayInterface) {
             $this->paymentGateway = $gateway;
-        }
-        else{
-            if($gateway){
+        } else {
+            if ($gateway) {
                 $gateway = config("ego.providers.$gateway");
-                if(!$gateway){
-                    throw new UnspportedGatewayException('Gateway not supported. If you can, please consider contributing to this package to enable it here.');
+                if (!$gateway) {
+                    throw new UnsupportedGatewayException('Gateway not supported. If you can, please consider contributing to this package to enable it here.');
                 }
-            }
-            else{
-                $gateway = config("ego.providers.".config("ego.default"));
+            } else {
+                $gateway = config("ego.providers." . config("ego.default"));
             }
             $this->paymentGateway = new $gateway();
         }
-        if($key){
+        if ($key) {
             $this->setKey($key);
         }
     }
@@ -38,30 +36,30 @@ class PaymentFactory implements PaymentGatewayInterface
         return $this->paymentGateway;
     }
     public function __call($name, $arguments)
-	{
+    {
         return $this->paymentGateway->$name(...$arguments);
-	}
+    }
     public function setKey(string|array $key): void
     {
         $this->paymentGateway->setKey($key);
     }
-    public function getPayload():array
-	{
-		return $this->paymentGateway->getPayload();
-	}
+    public function getPayload(): array
+    {
+        return $this->paymentGateway->getPayload();
+    }
     public function prepareForPayment(array $array): array
     {
         return $this->paymentGateway->prepareForPayment($array);
     }
-    public function pay(array $array=[]): array
+    public function pay(array $array = []): array
     {
         return $this->paymentGateway->pay($array);
     }
-    public function transfer(array $data=[]): array
+    public function transfer(array $data = []): array
     {
         return $this->paymentGateway->transfer($data);
     }
-    public function verifyPayment(array|string $reference=[]): array
+    public function verifyPayment(array|string $reference = []): array
     {
         return $this->paymentGateway->verifyPayment($reference);
     }
@@ -69,12 +67,12 @@ class PaymentFactory implements PaymentGatewayInterface
     {
         $this->paymentGateway->verifyWebhook($request);
     }
-    public function getBanks(string $countryCode=""): array
-	{
+    public function getBanks(string $countryCode = ""): array
+    {
         return $this->paymentGateway->getBanks($countryCode);
     }
-    public function verifyAccountNumber(array $request=[]): array
-	{
+    public function verifyAccountNumber(array $request = []): array
+    {
         return $this->paymentGateway->verifyAccountNumber($request);
     }
 }
